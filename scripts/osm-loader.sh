a875f5047026d5863b4dcafaba0521cc2498720a
Christian Landgren <cl@iteam.se>
2016-10-14 13:30:57 +0200

    download swedish osm

--- scripts/osm-loader.sh
+++ scripts/osm-loader.sh
@@ -7,6 +7,6 @@ mkdir -p $DATA/openstreetmap
 cd $DATA/openstreetmap
 
 # Download osm data
-curl -sS -O http://download.geofabrik.de/europe/finland-latest.osm.pbf
+curl -sS -O http://download.geofabrik.de/europe/sweden-latest.osm.pbf
 
 echo 'OK' >> /tmp/loadresults
